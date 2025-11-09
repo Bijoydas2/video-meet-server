d9fe513d1995b6802b006d8e97533127587ae50c
Bijoydas2 <bijoydas5680@gmail.com>
2025-11-09 20:18:49 +0600

    create backend file and connect mongodb

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+console.log("Uploads path:", path.join(__dirname, "uploads"));
+
+
+const uri = process.env.MONGO_URL;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log(" Connected to MongoDB");
+
+    const db = client.db("video-meet");
+
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+  }
+}
+
+run().catch(console.error);
+
+
+app.get("/", (req, res) => {
+  res.send("Video meet API is running...");
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
