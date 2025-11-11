f55e3534eef5b258573319a391cbca21886e0af6
Bijoydas2 <bijoydas5680@gmail.com>
2025-11-11 21:00:26 +0600

    again update cors oriin and fixed deploy issue

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ["https://video-meet-client-seven.vercel.app"],
-  credentials: true
+  origin: ["https://video-meet-client-seven.vercel.app", "http://localhost:5173"],
+ credentials: true
 }));
 app.use(express.json());
 
